44a6dd45c7f0d5d9c9badd3c8bb0ee4dd3603519
Allen Rettberg <arettberg@gmail.com>
2016-12-21 18:43:09 -0800

    front-end interface

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    @twitter_username = params[:username]
+    @twitter_user = TwitterInterface.get_user @twitter_username
 
+    if @twitter_user
+      @tweets = TwitterInterface.recent_tweets_for_user(@twitter_user)
+    end
   end
 end
